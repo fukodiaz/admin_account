1de532c2ce7461444acff822ac401969312843d0
Fuko <fukodiaz@gmail.com>
2022-07-12 15:50:56 +0700

    included and used react-select in the pop-up of users block

--- src/components/form-user/form-user.js
+++ src/components/form-user/form-user.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Select from 'react-select';
 
+import SelectItems from '../select-items';
 import styles from './form-user.m.less'
 
 const FormUser = (props) => {
@@ -8,91 +8,6 @@ const FormUser = (props) => {
 			dataOptionsPositions, position, department} = props;
 	const contentHeading = heading === 'headingNewUser' ? 
 				<span>Добавить сотрудника <br /> выбранного управления</span> : null;
-	const dot = (color = 'rgba(0,0,0,.7)') => ({
-		color,
-		fontSize: '15px',
-		paddingLeft: '1px',
-		paddingRight: 20
-	});
-
-	const selectStyles = (height=104) => ({
-		input: styles => ({...styles, ...dot()}),
-		placeholder: styles => ({...styles, ...dot()}),
-		indicatorSeparator: (styles) => ({display:'none'}),
-		control: (base, state) => ({
-			...base,
-			position: 'relative',
-			boxShadow: 'none',
-			border: state.isFocused ? '1px solid rgba(23, 135, 247, 0.4)' 
-											: '1px solid rgba(160, 158, 158, 0.7)',
-			outline: state.isFocused ? '2px solid rgba(23, 135, 247, 0.4) !important' : 'none',
-			outlineOffset: '-2px',
-			'&:hover': {
-				borderColor: state.isFocused ? 'rgba(23, 135, 247, 0.4)' 
-									: 'rgba(138, 136, 136, 0.4) !important',
-				outline: state.isFocused ? '2px solid rgba(23, 135, 247, 0.4) !important' 
-													:'1px solid rgba(138, 136, 136, 0.4) !important',
-			},
-
-			':after, :before': {
-				content:'""',
-				position: 'absolute',
-				zIndex: 999,
-				top: 19,
-				right: 6,
-				width: 9,
-				height: 2,
-				transform: 'rotate(-45deg)',
-				backgroundColor: state.isFocused ? 'rgb(163,163,163)' : '#cccccc',
-				'&:hover': {
-					backgroundColor: 'rgb(163,163,163)'
-				}
-			},
-			':before': {
-				right: 12,
-				transform: 'rotate(45deg)',
-			},
-		}),
-		dropdownIndicator: base => ({
-			...base,
-			display:'none'
-		}),
-		menu: (provided, state) => ({
-			...provided,
-			width: '100%',
-			padding: 0,
-			fontSize: '15px',
-		}),
-		menuList: base => ({
-			...base,
-			maxHeight: height//104, 
-		}),
-		singleValue: (provided) => {
-			return { ...provided, ...dot(), color:'#000' };
-		},
-		option: (provided, state) => ({
-			...provided,
-			color: '#000',
-			lineHeight: '20px',
-			backgroundColor: state.isSelected ? 'rgba(160, 158, 158, 0.2)' : '#fff',
-			padding: '9px 11px 10px',
-			margin: 0,
-			borderBottom: '1px solid rgba(160, 158, 158, 0.4)',
-			borderTop: '1px solid rgba(160, 158, 158, 0.1)',
-			':first-child': {
-				borderTop: 'none'
-			},
-			':last-child': {
-				borderBottom: 'none'
-			},
-			'&:hover': {
-				backgroundColor: 'rgba(160, 158, 158, 0.2)'
-			}
-		}),
-	});
-
-	const stylesPositions = selectStyles(167);
-	const stylesDepartments = selectStyles();
 	
 	return (
 		<div className={styles.boxFormUser}>
@@ -110,12 +25,10 @@ const FormUser = (props) => {
 									placeholder="ФИО" />
 					</div>
 					<div className={styles.containerSelectPosition}>
-						<Select 	value={position} name="position_user"
-									options={dataOptionsPositions}
-									onChange={(opt)=> inputChanged('position_user', opt)}
-									placeholder="Должность" className={styles.selectPosition}
-									styles={stylesPositions} isSearchable={false}>
-						</Select>
+						<SelectItems 	value={position} name="position_user"
+									dataOptions={dataOptionsPositions}
+									inputChanged={inputChanged} height={167}
+									placeholder="Должность" isSearchable={false} />
 					</div>
 					<div className={styles.containerInputEmail}>
 						<input type="email" name="email_user" 
@@ -132,13 +45,10 @@ const FormUser = (props) => {
 									placeholder="Телефон" />
 					</div>
 					<div className={styles.containerSelectDepartment}>
-						<Select 	value={department} name="department_user"
-									options={dataOptionsDepartments}
-									onChange={(opt)=> inputChanged('department_user', opt)}
-									placeholder="Подразделение" className={styles.selectDepartment}
-									styles={stylesDepartments} isSearchable={false}
-									menuListHeight='120px'>
-						</Select>
+						<SelectItems 	value={department} name="department_user"
+									dataOptions={dataOptionsDepartments}
+									inputChanged={inputChanged} isSearchable={false}
+									placeholder="Подразделение" />
 					</div>
 					<div className={styles.containerInputPassword}>
 						<input type="password" name="password_user" 
